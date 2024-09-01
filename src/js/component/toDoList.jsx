f27e0ef3e870d7f44df2d1181d2838d89ee9e01c
FidelNieto <nietofidel25@gmail.com>
2024-08-31 20:35:42 -0400

    adding fetch, works to add and delete

--- src/js/component/toDoList.jsx
+++ src/js/component/toDoList.jsx
@@ -1,38 +1,95 @@
-import React, {useState, useEffect} from "react"
+import React, { useState, useEffect } from "react";
 
+function ToDoList() {
+  const [inputValue, setInputValue] = useState("");
+  const [toDo, setToDo] = useState([]);
 
-function ToDoList(){
-    const [inputValue, setInputValue] = useState("")
-    const [toDo, setToDo] = useState([])
+  function handleOnChange(event) {
+    setInputValue(event.target.value);
+  }
 
-    function handleOnChange(event){
-        setInputValue(event.target.value)
-    }
+  async function fetchAccountTodos() {
+    try {
+      const todos = await fetch(
+        "https://playground.4geeks.com/todo/users/fidelnieto25",
+        { method: "GET" }
+      );
+      const translatedTodos = await todos.json();
+      console.log(translatedTodos);
+      setToDo(translatedTodos.todos);
+    } catch {}
+  }
 
-function insert(){
-    const trimmedValue = inputValue.trim(); 
+  useEffect(fetchAccountTodos, []);
+
+  async function insertOnClick() {
+    try {
+      const trimmedValue = inputValue.trim();
       if (trimmedValue === "") {
         return;
       }
-
-      setToDo([...toDo, inputValue]);
+      await fetch("https://playground.4geeks.com/todo/todos/fidelnieto25", {
+        method: "POST",
+        body: JSON.stringify({
+          label: inputValue,
+          is_done: false,
+        }),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      });
+      await fetchAccountTodos();
       setInputValue("");
-}
-    
+    } catch (error) {}
+  }
 
-    return <div>
-        <input onChange={handleOnChange} value={inputValue} type="text" name="todo" id="todo" placeholder="What we need to do?" />
-        <button onClick={insert}>Incluir toDo</button>
+  // Remove ON CLICK BUT, prompting the ID
+  //   async function removeOnClick() {
+  //     try {
+  //       const todo_id = prompt("Cual tarea deseas borrar?");
+  //       await fetch(`https://playground.4geeks.com/todo/todos/${todo_id}`, {
+  //         method: "DELETE",
+  //       });
+  //       await fetchAccountTodos();
+  //     } catch (error) {}
+  //   }
 
-        <div>
-            <ul>
-                {toDo.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}    
-            </ul>
-        
-        </div>
+  async function removeOnClick(id) {
+    try {
+      await fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+        method: "DELETE",
+      });
+      await fetchAccountTodos();
+    } catch (error) {}
+  }
+
+  return (
+    <div>
+      <input
+        onChange={handleOnChange}
+        value={inputValue}
+        type="text"
+        name="todo"
+        id="todo"
+        placeholder="What we need to do?"
+      />
+      <button onClick={insertOnClick}>Incluir toDo</button>
+
+      <div>
+        <ul className="list-group d-flex w-100 justify-content-between">
+          {toDo.map((item, index) => (
+            <li className="list-group-item" key={index}>
+              {item.label}
+              <button onClick={() => removeOnClick(item.id)} className="btn">
+                x
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
+  );
 }
 
-export default ToDoList
+export default ToDoList;
